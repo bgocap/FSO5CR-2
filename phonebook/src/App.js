9eb3d382b7adb60e42c9f9c4c2960d3f647e6e52
bgocap <diego.capcha98@hotmail.com>
2023-08-02 14:58:49 +0300

    Exercise 2.9

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,16 +1,28 @@
-import { useState } from 'react'
+import { cloneElement, useState } from 'react'
+
+const Contact = ({contact}) => <p>{contact.name} {contact.number}</p> 
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '0401234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-
+  const [filter, setFilter] = useState('')
+  
+  const filteredPersons=
+  persons.filter(filteredPersons=>
+    filteredPersons.name.toLowerCase().match(filter.toLowerCase()))
+  
   const addPerson = (event) => {
 
     event.preventDefault()
-    const personObject = {name:newName, number:newNumber}
+    const newId=persons.reduce((id,item)=>id=item.id+1,0)
+    const personObject = {name:newName, number:newNumber, id:newId}
  
     persons.some(person=>person.name===newName)?
     alert(`${newName} is already added to phonebook`)
@@ -28,9 +40,15 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div> filter shown with: <input value={filter} onChange={handleFilterChange} /> </div>
+      <h3>Add a new</h3>
       <form onSubmit={addPerson}>
         <div> name: <input value={newName} onChange={handleNameChange} /> </div>
         <div> number: <input value={newNumber} onChange={handleNumberChange} /> </div>
@@ -38,8 +56,10 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      <div>{persons.map(personsCopy=><p key={personsCopy.name}> {personsCopy.name} {personsCopy.number}</p>)}</div>
+      <h3>Numbers</h3>
+      <div>{filteredPersons.map(filteredPerson=>
+          <Contact key={filteredPerson.id} contact={filteredPerson}/>
+        )}</div>
     </div>
   )
 }
